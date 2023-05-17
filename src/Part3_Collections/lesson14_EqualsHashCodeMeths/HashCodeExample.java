f20db70eee063f89500a6de5577b558370c910c5
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-05-17 17:20:13 +0300

    Collections: HashCode Example

---
+++ src/Part3_Collections/lesson14_EqualsHashCodeMeths/HashCodeExample.java
@@ -0,0 +1,71 @@
+package Part3_Collections.lesson14_EqualsHashCodeMeths;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HashCodeExample {
+
+    public static void main(String[] args) {
+
+        Map<Obj, Double> map = new HashMap<>();
+        Obj obj1 = new Obj("A", "aaa", 1);
+        Obj obj2 = new Obj("B", "bbb", 2);
+        Obj obj3 = new Obj("C", "ccc", 3);
+        map.put(obj1, 0.5);
+        map.put(obj2, 1.0);
+        map.put(obj3, 1.5);
+        System.out.println(map);
+
+        Obj obj = new Obj("A", "aaa", 1);
+        System.out.println(obj.equals(obj1));
+        System.out.println(map.containsKey(obj));
+
+        System.out.println(obj.hashCode());
+        System.out.println(obj1.hashCode());
+        System.out.println(obj2.hashCode());
+        System.out.println(obj3.hashCode());
+    }
+}
+
+class Obj {
+    String name;
+    String bio;
+    int id;
+
+
+    public Obj(String name, String bio, int id) {
+        this.name = name;
+        this.bio = bio;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Obj{" +
+                "name='" + name + '\'' +
+                ", bio='" + bio + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Obj obj = (Obj) o;
+        return id == obj.id && Objects.equals(name, obj.name) && Objects.equals(bio, obj.bio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, bio, id);
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return name.length() * 7 + bio.length() * 11 + id * 53;
+//    }
+
+}
+
