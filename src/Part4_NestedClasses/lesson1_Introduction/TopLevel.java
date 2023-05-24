977283049ffe70b1c7e876ba06f3f60c8a388d63
Yandr2022 <sharkievich@gmail.com>
2023-05-24 06:25:53 +0300

    Nested classes: introduction

---
+++ src/Part4_NestedClasses/lesson1_Introduction/TopLevel.java
@@ -0,0 +1,14 @@
+package Part4_NestedClasses.lesson1_Introduction;
+
+public class TopLevel {
+    static class Nested {
+    }
+
+    class Inner {
+    }
+
+    void foo() {
+        class Locale {
+        }
+    }
+}
