2130eb8f04739a6ca5fe319a32d2c8d469dac464
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-06-14 17:41:07 +0300

    SMultithreading: Introduction

---
+++ src/Part7_Multithreading/lesson1_Introduction/Demo.java
@@ -0,0 +1,11 @@
+package Part7_Multithreading.lesson1_Introduction;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("start");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("end");
+    }
+}
