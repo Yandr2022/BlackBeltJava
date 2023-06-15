488865f51aebd599133dae8d3905f0cd80f38edf
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-06-15 17:49:49 +0300

    Multithreading: volatile

---
+++ src/Part7_Multithreading/lesson6_KWVolatile/Demo.java
@@ -0,0 +1,15 @@
+package Part7_Multithreading.lesson6_KWVolatile;
+
+public class Demo extends Thread{
+
+    boolean flag = true;
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
