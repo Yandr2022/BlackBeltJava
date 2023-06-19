e53a9baec1d1c3196917b3b60558db3433ae227f
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-06-19 18:03:25 +0300

    Multithreading: volatile

--- src/Part7_Multithreading/lesson6_KWVolatile/Demo.java
+++ src/Part7_Multithreading/lesson6_KWVolatile/Demo.java
@@ -1,15 +1,30 @@
 package Part7_Multithreading.lesson6_KWVolatile;
 
-public class Demo extends Thread{
+public class Demo extends Thread {
 
-    boolean flag = true;
+    volatile boolean  flag = true;
 
     @Override
     public void run() {
-        super.run();
+        long counter = 0;
+        while (flag) {
+            counter++;
+        }
+        System.out.println("loop is finished. Counter = " + counter);
     }
 
     public static void main(String[] args) {
+        Demo thread = new Demo();
+        thread.start();
+        try {
+            Thread.sleep(3000);
+            System.out.println("main thread wake up after 3 sec");
+            thread.flag = false;
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        System.out.println("end of program");
     }
 }
