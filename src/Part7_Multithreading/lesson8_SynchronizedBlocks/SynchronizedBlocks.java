aed21ec4464e4128f8ee7f7e13f864f572e3a751
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-06-22 17:57:00 +0300

    Multithreading: synchronized blocks, monitor: part1

---
+++ src/Part7_Multithreading/lesson8_SynchronizedBlocks/SynchronizedBlocks.java
@@ -0,0 +1,44 @@
+package Part7_Multithreading.lesson8_SynchronizedBlocks;
+
+public class SynchronizedBlocks {
+    public static void main(String[] args) {
+        MyRunnable runnable = new MyRunnable();
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
+class Counter {
+    static int count = 0;
+
+
+    public static synchronized void increment() {
+        System.out.print(Counter.count++ + " ");
+    }
+}
+
+class MyRunnable implements Runnable {
+
+    private void labelPrint(){
+        System.out.println("not synchronized block");
+    }
+
+    private  void increment() {
+        labelPrint();
+        synchronized (this) {
+            System.out.print(Counter.count++ + " ");
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+//            Counter.increment();
+            increment();
+        }
+    }
+}
---
+++ src/Part7_Multithreading/lesson8_SynchronizedBlocks/SynchronyzedBlocks1.java
@@ -0,0 +1,49 @@
+package Part7_Multithreading.lesson8_SynchronizedBlocks;
+
+
+import static Part7_Multithreading.lesson8_SynchronizedBlocks.SynchronyzedBlocks1.increment;
+
+public class SynchronyzedBlocks1 {
+    volatile static int counter = 0;//or not - optional
+    volatile static int counter1 = 0;//or not - optional
+
+    public static synchronized void increment() {
+        counter++;
+    }
+    public static void increment1() {
+        synchronized (SynchronyzedBlocks1.class) {
+            counter1++;
+        }
+    }
+
+
+    public static void main(String[] args) {
+         Thread thread = new Thread(()->{ for (int i = 0; i < 1000; i++) {
+//             increment();
+           increment1();
+        }});
+        Thread thread1= new Thread(()->{ for (int i = 0; i < 1000; i++) {
+//             increment();
+            increment1();
+        }});
+
+        thread.start();
+        thread1.start();
+
+
+
+        try {
+            thread.join();
+            thread1.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(counter);
+        System.out.println(counter1);
+
+    }
+}
+
+
