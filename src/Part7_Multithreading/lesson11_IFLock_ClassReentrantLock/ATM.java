d6ea5f4fc4882fbb4ea2be4d06d0be7273cd5fc2
Yandr2022 <sharkievich@gmail.com>
2023-06-25 09:50:42 +0300

    Multithreading: IF Lock, class ReentrantLock

---
+++ src/Part7_Multithreading/lesson11_IFLock_ClassReentrantLock/ATM.java
@@ -0,0 +1,50 @@
+package Part7_Multithreading.lesson11_IFLock_ClassReentrantLock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ATM {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Lock lock = new ReentrantLock();
+
+        new Employee("Yuli",lock);
+        new Employee("Ivan",lock);
+        new Employee("Alex",lock);
+        Thread.sleep(5000);
+        new Employee("Natali",lock);
+        new Employee("Igor",lock);
+
+    }
+}
+
+class Employee extends Thread{
+    private String name;
+    private Lock lock;
+
+    public Employee(String name, Lock lock) {
+        this.name = name;
+        this.lock = lock;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        if(lock.tryLock()) {
+            try {
+//            System.out.println(name + " waiting....");
+//            lock.lock();
+                System.out.println(name + " using ATM now");
+                Thread.sleep(2000);
+                System.out.println(name + " finished using ATM");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }else{
+            System.out.println(name+" doesn't want to wait");
+        }
+    }
+}
--- src/Part7_Multithreading/lesson11_IFLock_ClassReentrantLock/Demo.java
+++ src/Part7_Multithreading/lesson11_IFLock_ClassReentrantLock/Demo.java
@@ -6,11 +6,54 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Demo {
 
     public static void main(String[] args) {
+        Thread threads[] = {new Thread(Call::mobileCall),new Thread(Call::skypeCall),new Thread(Call::whatsappCall)};
 
+        for (Thread thread:threads) {
+            thread.start();
+        }
     }
 }
 
-class Call{
+class Call {
 
-    private Lock lock = new ReentrantLock();
+    private static Lock lock = new ReentrantLock();
+
+    static void mobileCall() {
+        lock.lock();
+        try {
+            System.out.println("mobile call starts");
+            Thread.sleep(3000);
+            System.out.println("mobile call ends");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    static void skypeCall() {
+        lock.lock();
+        try {
+            System.out.println("skype call starts");
+            Thread.sleep(5000);
+            System.out.println("skype call ends");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    static void whatsappCall() {
+        lock.lock();
+        try {
+            System.out.println("whatsapp call starts");
+            Thread.sleep(2000);
+            System.out.println("whatsapp call ends");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
 }
