f262c09159d7cae4b8557e9d4f41e1662336850f
Yandr2022 <sharkievich@gmail.com>
2023-07-01 07:24:24 +0300

    IO NIO: buffered Reader & Buffered Writer

--- src/Part8_IO_NIO_Files/lesson4_BufferReader_BufferWriter/Demo.java
+++ src/Part8_IO_NIO_Files/lesson4_BufferReader_BufferWriter/Demo.java
@@ -1,8 +1,28 @@
 package Part8_IO_NIO_Files.lesson4_BufferReader_BufferWriter;
 
+import java.io.*;
+
 public class Demo {
 
     public static void main(String[] args) {
 
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter("src/Part8_IO_NIO_Files/lesson4_BufferReader_BufferWriter/test.txt"));
+             BufferedReader reader = new BufferedReader(
+                     new FileReader("src/Part8_IO_NIO_Files/lesson1_StreamsIntroduction_FileWriter/test2.txt"))) {
+
+//            int character;
+//            while ((character = reader.read()) != -1) {
+//               writer.write((char) character);
+//            }
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
